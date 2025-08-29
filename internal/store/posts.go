dc9205c40ab3a0a4ba9b0ab1efb3b1c7641f9424
Nick <n.korelskiy@hotmail.com>
2025-08-29 18:23:07 +0300

    Pagination

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -18,11 +18,19 @@ type Post struct {
 	UpdatedAt string `json:"updated_at"`
 	Version int `json:"version"`
 	Comments []Comment `json:"comments"`
+	User User `json:"user"`
+}
+
+type PostWithMetadata struct {
+	Post
+	CommentCount int `json:"comments_count"`
 }
 
 type PostStore struct {
 	db *sql.DB
 }
+
+
 
 func (s *PostStore) Create(ctx context.Context, post *Post) error {
 	query := `
@@ -51,6 +59,9 @@ func (s *PostStore) Create(ctx context.Context, post *Post) error {
 
 	return nil
 }
+
+
+
 
 func (s *PostStore) GetByID(ctx context.Context, id int64) (*Post, error) {
 	query := `
@@ -86,6 +97,8 @@ func (s *PostStore) GetByID(ctx context.Context, id int64) (*Post, error) {
 	return &post, nil
 }
 
+
+
 func (s *PostStore) Delete(ctx context.Context, postID int64) error {
 	query := `DELETE FROM posts WHERE id = $1`
 
@@ -108,6 +121,8 @@ func (s *PostStore) Delete(ctx context.Context, postID int64) error {
 
 	return nil
 }
+
+
 
 func (s *PostStore) Update(ctx context.Context, post *Post) error {
 	query := `
@@ -132,4 +147,56 @@ func (s *PostStore) Update(ctx context.Context, post *Post) error {
 	}
 
 	return nil
+}
+
+
+func (s *PostStore) GetUserFeed(ctx context.Context, userID int64, fq PaginatedFeedQuery) ([]PostWithMetadata, error) {
+	query := `
+	SELECT
+		p.id, p.user_id, p.title, p.content, p.created_at, p.version, p.tags,
+		u.username,
+		COUNT(c.id) AS comments_count
+	FROM posts p
+	LEFT JOIN comments c ON c.post_id = p.id
+	LEFT JOIN users u ON p.user_id = u.id
+	LEFT JOIN followers f ON f.follower_id = $1
+	WHERE (f.user_id = p.user_id OR p.user_id = $1)
+	GROUP BY p.id, u.username
+	ORDER BY p.created_at ` + fq.Sort + `
+	LIMIT $2 OFFSET $3
+	`
+
+	
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
+	rows, err := s.db.QueryContext(ctx, query, userID, fq.Limit, fq.Offset)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var feed []PostWithMetadata
+	for rows.Next() {
+		var p PostWithMetadata
+		err := rows.Scan(
+			&p.ID, 
+			&p.UserID, 
+			&p.Title, 
+			&p.Content, 
+			&p.CreatedAt, 
+			&p.Version,
+			pq.Array(&p.Tags),
+			&p.User.Username,
+			&p.CommentCount,
+		)
+		if err != nil {
+		return nil, err
+		}
+		
+		feed = append(feed, p)
+	}
+
+	return feed, nil
 }
